629a76542ca0ee8609d0322d2e87d5fcc0024ff4
Oros <oros@ecirtam.net>
2023-10-02 11:29:53 +0200

    add lineage_lime

---
+++ lineage_lime.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from lime device
+$(call inherit-product, device/xiaomi/chime/device-lime.mk)
+
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_lime
+PRODUCT_DEVICE := lime
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 9T
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
